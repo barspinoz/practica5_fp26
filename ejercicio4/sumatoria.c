3cd7bf3e2fa417952fa4e1eb87029a46b561f069
barspinoz <109839725+barspinoz@users.noreply.github.com>
2025-09-28 16:14:09 -0600

    Algoritmo de sumatoria

--- ejercicio4/sumatoria.c
+++ ejercicio4/sumatoria.c
@@ -1,1 +1,24 @@
-
+#include <stdio.h>
+#include <math.h>
+int main() {
+	float N = 0; //numero maximo
+	float n = 1; //entrada
+	float suma = 0; //salida
+	float termino = 0; //intermedia
+	
+	/*leer valores*/
+	printf("\n Ingrese un valor maximo N: \n \t");
+	scanf("%f",&N);
+	printf("\n Ingrese un valor n: \n \t");
+	scanf("%f", &n);
+	
+	/*calculo*/
+	if(n <= N) {
+		termino = (pow(-1, n)/(n));
+		suma = suma + termino;
+		n = n++;
+	}
+	
+	printf("\n La sumatoria es: %f \n", &suma);
+	return 0;
+}
